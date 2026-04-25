6931f75456f9b08a67a2cbbb083fef75e57e098f
SADAB AZHAR <sadabazhar84@gmail.com>
2026-04-25 12:41:19 +0530

    chore: initial project setup

---
+++ src/main/java/com/grid07/coreapi/guardrails/CoreApiGuardrailsApplication.java
@@ -0,0 +1,13 @@
+package com.grid07.coreapi.guardrails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApiGuardrailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApiGuardrailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grid07/coreapi/guardrails/CoreApiGuardrailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grid07.coreapi.guardrails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApiGuardrailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
